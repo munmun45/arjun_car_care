3f9761088b706f78d450e012eda758df18a29e5b
munmun45 <manaranamunmun@gmail.com>
2025-08-20 17:06:58 +0530

    sdf

--- somaspanel/config/menu.php
+++ somaspanel/config/menu.php
@@ -17,51 +17,17 @@
                 <span>Booking Manager</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-wallet2"></i>
-                <span>Payment Gateway</span>
-            </a>
-        </li>
+        
 
         <li class="nav-heading">Customisation</li>
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="#">
                 <i class="bi bi-door-open"></i>
-                <span>Room Manager</span>
+                <span></span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-shop"></i>
-                <span>Restaurant Manager</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-tools"></i>
-                <span>Service Manager</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-boxes"></i>
-                <span>Facility</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-image"></i>
-                <span>Gallery</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#">
-                <i class="bi bi-chat-square-dots"></i>
-                <span>Contact</span>
-            </a>
-        </li>
+    
 
 
 
